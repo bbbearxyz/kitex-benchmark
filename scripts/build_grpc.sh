dad2fb4fbcbbb0663e1e9792573f5999235d6b23
bbbearxyz <mzj1996@mail.ustc.edu.cn>
2022-03-30 15:41:03 +0800

    修改scripts

--- scripts/build_grpc.sh
+++ scripts/build_grpc.sh
@@ -9,8 +9,10 @@ rm -rf $output_dir/log/ && mkdir -p $output_dir/log/
 # build kitex
 $GOEXEC mod edit -replace=github.com/apache/thrift=github.com/apache/thrift@v0.13.0
 $GOEXEC mod tidy
-$GOEXEC build -v -o $output_dir/bin/kitex_bencher $grpc_dir/kitex/client
-$GOEXEC build -v -o $output_dir/bin/kitex_reciever $grpc_dir/kitex
+$GOEXEC build -v -o $output_dir/bin/kitex_bencher $pb_dir/kitex/client
+$GOEXEC build -v -o $output_dir/bin/kitex-mux_bencher $pb_dir/kitex-mux/client
+$GOEXEC build -v -o $output_dir/bin/kitex_reciever $pb_dir/kitex
+$GOEXEC build -v -o $output_dir/bin/kitex-mux_reciever $pb_dir/kitex-mux
 
 # build others
 $GOEXEC mod edit -replace=github.com/apache/thrift=github.com/apache/thrift@v0.14.2
